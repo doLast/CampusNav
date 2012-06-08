b830f1166677e1e3efbc63d872d0e8887d71a022
Greg Wang <gregwym@qq.com>
2012-06-07 20:20:35 -0400

    Initial Commit

---
+++ CampusNav/CNAppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  CNAppDelegate.h
+//  CampusNav
+//
+//  Created by Greg Wang on 12-6-7.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CNAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
