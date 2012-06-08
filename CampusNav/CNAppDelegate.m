b18a55b6b14425534f101950694b6e43b508c2ca
Greg Wang <gregwym@qq.com>
2012-06-08 13:20:52 -0400

    Add Storyboard. Add basic UI design to Storyboard. Modify AppDelegate and project setting to use Storyboard.

--- CampusNav/CNAppDelegate.m
+++ CampusNav/CNAppDelegate.m
@@ -17,10 +17,12 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
-    self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
+//    self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
     // Override point for customization after application launch.
-    self.window.backgroundColor = [UIColor whiteColor];
-    [self.window makeKeyAndVisible];
+//    self.window.backgroundColor = [UIColor whiteColor];
+//    [self.window makeKeyAndVisible];
+	
+	// Do nothing and return YES to use Storyboard
     return YES;
 }
 
